9962ac8d36781462d9db76e933fa1db6b70696af
James Dent <semajdent@gmail.com>
2018-06-12 23:53:14 +1200

    Fixed implementation of zip_file::read_all() to perform a chunk-wise read rather than relying upon zip_ftell() to determine the size of the file.

--- src/libanki/zip_file.cpp
+++ src/libanki/zip_file.cpp
@@ -6,6 +6,7 @@
 
 #include "zip.h"
 
+#include <cassert>
 #include <cstdint>
 #include <cstddef>
 
@@ -30,29 +31,38 @@ namespace anki {
 
     auto zip_file::read_all() const -> std::vector<std::byte> {
 
+        static constexpr auto chunk_size = std::size_t{8192}; // Arbitrary; not profiled
+
         const auto handle = handle_cast(_handle);
         assert(handle);
 
-        if (zip_fseek(handle, 0, SEEK_END) != 0) {
-            throw_error(*handle);
-        }
+        auto bytes     = std::vector<std::byte>{};
+        auto pos       = std::size_t{0};
+        auto size_read = std::size_t{0};
 
-        const auto wide_size = std::int64_t{zip_ftell(handle)};
-        if (wide_size < 0) {
-            throw_error(*handle);
-        }
+        do {
 
-        if (zip_fseek(handle, 0, SEEK_SET) != 0) {
-            throw_error(*handle);
-        }
+            // Each chunk of `bytes` is unnecessarily zeroed before being populated with data read
+            // from the file; this seems unlikely to be a performance problem though.
 
-        const auto size = ksr::narrow_cast<std::size_t>(wide_size);
-        auto bytes = std::vector<std::byte>{};
-        bytes.resize(size);
+            pos = bytes.size();
+            bytes.resize(pos + chunk_size);
 
-        if (zip_fread(handle, bytes.data(), size) != 0) {
-            throw_error(*handle);
+            const auto wide_size_read = std::int64_t{zip_fread(handle, &bytes[pos], chunk_size)};
+            if (wide_size_read >= 0) {
+                size_read = ksr::narrow_cast<std::size_t>(wide_size_read);
+            }
+            else {
+                throw_error(*handle);
+            }
         }
+        while (size_read == chunk_size);
+
+        assert(size_read < chunk_size);
+        const auto unused_size = chunk_size - size_read;
+
+        assert(unused_size < bytes.size());
+        bytes.resize(bytes.size() - unused_size);
 
         return bytes;
     }
